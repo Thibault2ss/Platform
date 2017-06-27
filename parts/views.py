721743899e3f4bfc5240691c4dd8747a750c440f
Unknown <corthiclem@gmail.com>
2017-06-27 16:02:59 +0800

    Added path

--- parts/views.py
+++ parts/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from .models import SP3D_Part
+from django.utils.encoding import smart_str
 # Create your views here.
 
 def index(request):
@@ -14,5 +15,6 @@ def index(request):
 
 def download(request):
     response = HttpResponse(content_type='application/force-download')
-    response['Content-Disposition'] = 'attachment; filename=%s' % "/home/user01/SpareParts_Database/files/AMF/1.amf"
+    response['Content-Disposition'] = 'attachment; filename=%s' % smart_str("1.amf")
+    response['X-Sendfile'] = smart_str("/home/user01/SpareParts_Database/files/AMF/1.amf")
     return response
