2213ad8f45b2c087abb72f6ab628da6150125439
Unknown <corthiclem@gmail.com>
2017-06-27 16:12:17 +0800

    grr

--- parts/views.py
+++ parts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from .models import SP3D_Part
-from django.core.servers.basehttp import FileWrapper
+import os
 # Create your views here.
 
 def index(request):
@@ -14,8 +14,8 @@ def index(request):
     return render(request, 'parts/index.html', context)
 
 def download(request):
-    filename = "/home/user01/SpareParts_Database/files/AMF/1.amf" # Select your file here.                                
-    wrapper = FileWrapper(file(filename))
-    response = HttpResponse(wrapper, content_type='text/plain')
+    filename = "/home/user01/SpareParts_Database/files/AMF/1.amf"
+    response = HttpResponse(file(filename), content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(filename)
     response['Content-Length'] = os.path.getsize(filename)
     return response
