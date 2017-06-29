d8641dc939b3ea61eac1bb05f8e9554d5eb501cc
Unknown <corthiclem@gmail.com>
2017-06-29 10:54:21 +0800

    Added button for slicing and try the slice and download button

--- parts/urls.py
+++ parts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<id>[0-9]+)/download_amf/$', views.download_amf, name='download_amf'),
     url(r'^(?P<id>[0-9]+)/download_config/$', views.download_config, name='download_config'),
+    url(r'^(?P<id>[0-9]+)/slice_and_download/$', views.slice_and_download, name='slice_and_download'),
 ]
--- parts/views.py
+++ parts/views.py
@@ -26,3 +26,14 @@ def download_config(request, id):
     response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(filename)
     response['Content-Length'] = os.path.getsize(filename)
     return response
+
+def slice_and_download(request,id):
+    part = SP3D_Part.objects.get(id=id)
+    filename = "/home/user01/SpareParts_Database/files/AMF/" + part.amf + ".amf"
+    response = HttpResponse(file(filename), content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename=%s' % os.path.basename(filename)
+    response['Content-Length'] = os.path.getsize(filename)
+    return response
+    
+def slice_and_print(request, id):
+    return None
